f3c2565a0a03c968f852bafb5377469a8f9635ee
Omkar12683 <bagalomkar945@example.com>
2025-05-11 22:47:33 +0530

    files changes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
@@ -23,7 +24,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 
 // api's
@@ -31,6 +32,13 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
+});
 
 
 
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://bagalomkar945:EcsYQJelDy7CfBB5@cluster0.wytms5c.mongodb.net/job');
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
